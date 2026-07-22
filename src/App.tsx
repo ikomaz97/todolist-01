a120f8696471578c18f2c9d71b10f48c3d452e59
Chernov-Vpnpm <ikomaz97@gmail.com>
2026-07-22 18:13:40 +0400

    22.07.2026-06

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import './App.css'
-import {TodoList} from "./TodoList.tsx";
+import {TodolistItem} from './TodolistItem'
 
-export const  App = () => {
+export type Task = {
+  id: number
+  title: string
+  isDone: boolean
+}
+
+export const App = () => {
+  const tasks: Task[] = [
+    { id: 1, title: 'HTML&CSS', isDone: true },
+    { id: 2, title: 'JS', isDone: true },
+    { id: 3, title: 'ReactJS', isDone: false },
+    { id: 4, title: 'Redux', isDone: false },
+    { id: 5, title: 'Typescript', isDone: false },
+    { id: 6, title: 'RTK query', isDone: false },
+  ]
+
   return (
       <div className="app">
-
-         <TodoList title ="What to lern"/>
-         <TodoList title ="Song"/>
-         <TodoList title ="Books"/>
-
+        <TodolistItem title="What to learn" tasks={tasks} />
       </div>
   )
 }
-
-export default App
